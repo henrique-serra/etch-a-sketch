36bf0aa1d4bcbffbcd25a59127454d7e1dce73d4
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2024-12-16 15:02:16 +0000

    Fix issue about grid creation when user typed a number out of reach

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const btn = document.querySelector("button");
 const divContainer = document.querySelector("#container");
 
 function getLayout(msg = "How many squares per side for the grid? (Min 2; Max 100)") {
-    const layout = +prompt(msg);
+    let layout = +prompt(msg);
     if (!layout) {
         if (isNaN(layout)) {
-            getLayout("Type only numbers between 2 and 100.");
-            return;
+            layout = getLayout("Type only numbers between 2 and 100.");
         } else return;
     } else if (layout > 100) {
-        getLayout("You typed a number higher than 100. Choose a number between 2 and 100 of squares per side of the grid.");
-        return;
+        layout = getLayout("You typed a number higher than 100. Choose a number between 2 and 100 of squares per side of the grid.");
     } else if (layout < 2) {
-        getLayout("You typed a number smaller than 2. Choose a number between 2 and 100 of squares per side of the grid.");
-        return;
+        layout = getLayout("You typed a number smaller than 2. Choose a number between 2 and 100 of squares per side of the grid.");
     }
-    return layout;
+    return layout
 }
 
 function createSquares(n, row) {
@@ -43,6 +40,5 @@ function createGrid(nSquaresPerSide) {
 
 btn.addEventListener("click", () => {
     let layout = getLayout();
-    console.log(layout);
     createGrid(layout);
 })
